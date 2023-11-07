af3c8dd74c64bdf745f68d1b7d31f34db4e985bd
gssp4167 <gssp4167@gmail.com>
2023-11-07 20:50:54 +0530

    Added city middlewares

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const router=express.Router();
 
-const {CityController}=require('../../controllers')
-
+const {CityController}=require('../../controllers');
+const {CityMiddlewares}=require('../../middlewares');
 // console.log("Inside Airplane Routes");
 
 // /api/v1/airplanes POST
 router.post("/",
+            CityMiddlewares.validateCreateRequest,
             CityController.createCity);
 
 
