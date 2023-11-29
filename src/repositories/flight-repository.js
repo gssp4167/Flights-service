bb0dd807a5169f780a989d1982d116e9b0c84c64
gssp4167 <gssp4167@gmail.com>
2023-11-29 22:52:32 +0530

    Added complex joins to fetch all airports

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,6 @@
 const CrudRepository = require('./crud-repository');
-const { Flight,Airplane } = require('../models');
-
+const { Flight,Airplane,Airport } = require('../models');
+const {Sequelize}=require('sequelize');
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -11,10 +11,31 @@ class FlightRepository extends CrudRepository {
         const response=await Flight.findAll({
             where:filter,
             order:sort,
-            include:{
-               model: Airplane,
-               required:true
-            }
+            include:[
+                {
+                    model: Airplane,
+                    required:true,
+                    as:'airplaneDetail'
+                },
+                {
+                    model:Airport,
+                    required:true,
+                    as:'departureAirport',
+                    on:{
+                        col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"),"=",Sequelize.col("departureAirport.code"))
+                    }
+                    
+                },
+                {
+                    model:Airport,
+                    required:true,
+                    as:'arrivalAirport',
+                    on:{
+                        col1:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"),"=",Sequelize.col("arrivalAirport.code"))
+                    }
+                    
+                }
+            ]
         });
         return response;
     }
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -59,7 +59,7 @@ async function getAllFlights(query){
         return flights;
     }
     catch(error){
-        console.log(error);
+        // console.log(error);
         throw new AppError('Cannot fetch data of all the flights',StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
